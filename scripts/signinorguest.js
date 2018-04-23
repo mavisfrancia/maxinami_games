81aee273f01ec39162afc4a8e3d56a62b9cbf450
Mavis Francia <mavis.francia@gmail.com>
2018-04-23 15:46:35 -0500

    Implement "Sign in or Checkout as guest"

---
+++ scripts/signinorguest.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#sign-in-btn").click(function() {
+		location.href = "signIn.php";
+	});
+
+	$("#continue-guest-btn").click(function() {
+		location.href = "checkout.php";
+	});
+
+});
+
